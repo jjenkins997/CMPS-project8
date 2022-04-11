50c4f726cb383ecd563c3c9907b039e068a3139d
Jahzah <c00227447@louisiana.edu>
2022-04-11 13:19:55 -0500

    Committing to view changes

---
+++ p8_c00227447_c00409618/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace p8_c00227447_c00409618.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
